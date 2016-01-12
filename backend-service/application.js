6e71364cba4765b7d4eb72f62a1b039438be8f4e
Charles Moulliard <ch007m@gmail.com>
2016-01-12 17:45:33 +0100

    Setup port for android

--- backend-service/application.js
+++ backend-service/application.js
@@ -1,14 +1,27 @@
-var mbaasApi = require('fh-mbaas-api');
+// var mbaasApi = require('fh-mbaas-api');
+// var mbaasExpress = mbaasApi.mbaasExpress();
+
 var express = require('express');
-var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var methodOverride  = require('method-override');
 
 var blogService = require('./lib/articles');
 
+var initData = {
+  domain: "testing",
+  firstTime: false,
+  hosts: {
+    //"url": 'http://localhost:8001'
+    "url": 'http://10.0.2.2:8001'
+  },
+  init: {
+    "trackId": "testtrackid"
+  }
+}
+
 // list the endpoints which you want to make securable here
-var securableEndpoints;
+// var securableEndpoints;
 // securableEndpoints = ['/articles'];
 
 var app = express();
@@ -24,19 +37,18 @@ app.use(methodOverride());      // simulate DELETE and PUT
 app.use(cors());
 
 // Note: the order which we add middleware to Express here is important!
-app.use('/sys', mbaasExpress.sys(securableEndpoints));
-app.use('/mbaas', mbaasExpress.mbaas);
+// app.use('/sys', mbaasExpress.sys(securableEndpoints));
+// app.use('/mbaas', mbaasExpress.mbaas);
 
 // allow serving of static files from the public directory
 app.use(express.static(__dirname + '/public'));
 
 // Note: important that this is added just before your own Routes
-app.use(mbaasExpress.fhmiddleware());
+// app.use(mbaasExpress.fhmiddleware());
 
-app.use('/box/srv/1.1/app/init', function(req, res){
-  res.json({
-    hosts: {url: 'http://localhost:8001'}
-  });
+app.use("/box/srv/1.1/app/init", function(req, res){
+  console.log("Got request for app init");
+  res.send(JSON.stringify(initData));
 });
 
 app.get('/articles', blogService.findAll);
@@ -45,7 +57,7 @@ app.get('/articles/searchuser/:user', blogService.findByUser)
 app.post('/articles', blogService.newPost);
 
 // Important that this is last!
-app.use(mbaasExpress.errorHandler());
+// app.use(mbaasExpress.errorHandler());
 
 var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
