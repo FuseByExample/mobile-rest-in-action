7c5cc927a879027743d09f35db787817ca37bb68
Charles Moulliard <ch007m@gmail.com>
2016-01-12 18:32:39 +0100

    Add the correct mapping between the key and the backend

--- backend-service/Gruntfile.js
+++ backend-service/Gruntfile.js
@@ -57,8 +57,7 @@ module.exports = function(grunt) {
            * or a remote instance.
            */
           var serviceMap = {
-            '0123456789ABCDEFGHIJKLMN': 'http://127.0.0.1:8001',
-            'SERVICE_GUID_2': 'https://host-and-path-to-service'
+            'fusekey001': 'http://127.0.0.1:8001'
           };
           return JSON.stringify(serviceMap);
         }
